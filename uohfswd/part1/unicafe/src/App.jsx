7089b290b71d2eb0b5d7187336c4478777e99afe
tsolawoyin <t.olawoyin@outlook.com>
2024-12-30 05:08:11 +0100

    Complete Unicafe

--- uohfswd/part1/unicafe/src/App.jsx
+++ uohfswd/part1/unicafe/src/App.jsx
@@ -1,35 +1,59 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+// A button component makes sense here of course.
+const Button = ({ type, val, update }) => {
+  // I can simply handle the functionality here simply...
+  function handleClick() {
+    update(val + 1); // nice and easy
+  }
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <button type="button" onClick={handleClick}>
+      {type}
+    </button>
+  );
+};
+
+const StatisticLine = ({text, value}) => {
+  return <p>{text} {value}{text == "Positive" && "%"}</p>
 }
 
-export default App
+// turn it into tables later on jare...
+const Statistics = ({ good, neutral, bad }) => {
+  const total = good + neutral + bad;
+  const average = (good / (good + neutral + bad)) * 100;
+  return (
+    <div>
+      <StatisticLine text={"Good"} value={good}/>
+      <StatisticLine text={"Neutral"} value={neutral}/>
+      <StatisticLine text={"Bad"} value={bad}/>
+      <StatisticLine text={"Average"} value={(good - bad) / total}/>
+      <StatisticLine text={"All"} value={total}/>
+      <StatisticLine text={"Positive"} value={average || 0}/>
+    </div>
+  );
+};
+
+const App = () => {
+  // nice and easy. Take it slow.
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  // nice states
+  return (
+    <div>
+      <h1>Give feedback</h1>
+      <div>
+        <Button type={"good"} val={good} update={setGood} />
+        <Button type={"neutral"} val={neutral} update={setNeutral} />
+        <Button type={"bad"} val={bad} update={setBad} />
+      </div>
+      <h2>Statistics</h2>
+      {good + bad + neutral != 0 ? (
+        <Statistics good={good} bad={bad} neutral={neutral} />
+      ) : <p><strong>No feedback given</strong></p>}
+    </div>
+  );
+};
+
+export default App;
